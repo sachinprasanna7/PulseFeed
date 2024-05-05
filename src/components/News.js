5f0e9afbdfe469a9d23e34a09c454b0dc2c1c1e0
Sachin <sachincodes1@gmail.com>
2024-05-05 06:56:04 +0530

    fixed some errors, finished app

--- src/components/News.js
+++ src/components/News.js
@@ -59,7 +59,7 @@ export class News extends Component {
     render() { 
         return (
             <div className="container my-3">
-                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '35px 0px' }}>PulseFeed - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner/>}
                 <div className="row"> 
                 {!this.state.loading && this.state.articles.map((element)=>{
